240b78dbd0c14008257eec68cc9bcb5f9ccc8bcd
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-04 16:56:56 +0200

    chore(vfio): update
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- modules/vfio/scripts/start.nix
+++ modules/vfio/scripts/start.nix
@@ -12,13 +12,13 @@ in
       then ''
         ${pkgs.systemd}/bin/systemctl stop lactd.service
         ${pkgs.systemd}/bin/systemctl stop display-manager.service
-        echo 0 > /sys/class/vtconsole/vtcon0/bind
-        echo 0 > /sys/class/vtconsole/vtcon1/bind
-        ${pkgs.kmod}/bin/modprobe -r amdgpu
-        sleep 1
-        ${pkgs.libvirt}/bin/virsh nodedev-detach pci_0000_03_00_0
-        ${pkgs.libvirt}/bin/virsh nodedev-detach pci_0000_03_00_1
-        ${pkgs.kmod}/bin/modprobe vfio-pci
+        # echo 0 > /sys/class/vtconsole/vtcon0/bind
+        # echo 0 > /sys/class/vtconsole/vtcon1/bind
+        # ${pkgs.kmod}/bin/modprobe -r amdgpu
+        # sleep 1
+        # ${pkgs.libvirt}/bin/virsh nodedev-detach pci_0000_03_00_0
+        # ${pkgs.libvirt}/bin/virsh nodedev-detach pci_0000_03_00_1
+        # ${pkgs.kmod}/bin/modprobe vfio-pci
       ''
       else ""
     }
--- modules/vfio/scripts/stop.nix
+++ modules/vfio/scripts/stop.nix
@@ -10,15 +10,14 @@ in
       ${
       if passthrough
       then ''
-        ${pkgs.libvirt}/bin/virsh nodedev-reattach pci_0000_03_00_0
-        ${pkgs.libvirt}/bin/virsh nodedev-reattach pci_0000_03_00_1
-        ${pkgs.kmod}/bin/modprobe -r vfio-pci
-        ${pkgs.kmod}/bin/modprobe amdgpu
-        echo 1 > /sys/class/vtconsole/vtcon0/bind
-        echo 1 > /sys/class/vtconsole/vtcon1/bind
+        # ${pkgs.libvirt}/bin/virsh nodedev-reattach pci_0000_03_00_0
+        # ${pkgs.libvirt}/bin/virsh nodedev-reattach pci_0000_03_00_1
+        # ${pkgs.kmod}/bin/modprobe -r vfio-pci
+        # ${pkgs.kmod}/bin/modprobe amdgpu
+        # echo 1 > /sys/class/vtconsole/vtcon0/bind
+        # echo 1 > /sys/class/vtconsole/vtcon1/bind
         ${pkgs.systemd}/bin/systemctl start lactd.service
         ${pkgs.systemd}/bin/systemctl start display-manager.service
-        ${pkgs.mullvad}/bin/mullvad connect
       ''
       else ""
     }
